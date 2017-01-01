4cbf1fba4eb804334d41a7feff79dfce7532b5d4
Joe Healey <jrj.healey@gmail.com>
2017-01-01 15:45:09 +0000

    Delete codon_skew.py

--- codon_skew.py
+++
@@ -1,8 +0,0 @@
-!/usr/bin/python
-
-# This script calculates the relative codon usage for a 
-# given supplied fasta file. The fasta should be a feature
-# as this script does not do anything beyond simply matching
-# and counting patterns/strings.
-
-
