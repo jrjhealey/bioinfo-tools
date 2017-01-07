d9aed7d4b6e4456002bbaf6c3308634a19e51d1e
Joe Healey <jrj.healey@gmail.com>
2017-01-07 14:13:26 +0000

    Create extractfeatures.py

---
+++ extractfeatures.py
@@ -0,0 +1,15 @@
+# Usage:
+# python splitgenbank.py multigenbank.gbk
+
+from Bio import SeqIO
+import sys
+
+with open(sys.argv[2], 'w') as ofh:
+        for seq_record in SeqIO.parse(sys.argv[1], 'genbank'):
+                for seq_feature in seq_record.features:
+                        if seq_feature.type=="CDS":
+                                assert len(seq_feature.qualifiers['translation'])==1
+                                ofh.write(">%s from %s\n%s\n" % (
+                                seq_feature.qualifiers['gene'][0],
+                                seq_record.name,
+                                seq_feature.qualifiers['translation'][0]))
