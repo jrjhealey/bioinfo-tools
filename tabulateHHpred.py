419bea9f2acc3fa377657d5a97b39bded2047526
jrjhealey <jrj.healey@gmail.com>
2017-05-02 18:03:19 +0100

    refactor tabulateHHpred

--- tabulateHHpred.py
+++ tabulateHHpred.py
@@ -1,25 +1,52 @@
 
-# This scripts takes the overly verbose HHpred outputs
-# and makes tabulated outputs
+"""
+This script takes the .hhr files output by HHSuite and
+turns the quite verbose file in to a fully tabulated
+version with all the fields separated one, one line per
+file. Thus, the file can be viewed simply in Excel etc.
 
-import os
-import subprocess
-import sys
-import argparse
-import traceback
-import warnings
-import pandas as pd
-from io import StringIO
-
-# Template of HHpred's verbose tables
-template = \
-u"""
----|------|------------------------|----|-------|-------|------|-----|----|---------|--------------|
- No Hit    Short Desc               Prob E-value P-value  Score    SS Cols Query HMM  Template HMM
+It requires the non-standard pandas module.
 """
 
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# The license for HHSuite remains with the authors. Please consult
+# their licensce agreements before using this software, and ensure
+# they are cited for any use. Run the script with -b to print rel-
+# evant references.
+
+
+import os
+import sys
+import traceback
+import warnings
+
+__author__ = "Joe R. J. Healey"
+__version__ = "1.0.0"
+__title__ = "tabulateHHpred"
+__license__ = "GPLv3"
+__author_email__ = "J.R.J.Healey@warwick.ac.uk"
+
+
 def hhparse(hhresult_file, verbose):
-	'''Convert HHpred's text-based output table in to an actual matrix'''
+	'''Convert HHpred's text-based output table in to a pandas dataframe'''
+	from io import StringIO
+	import pandas as pd
+
+	template = \
+	u"""
+	---|------|------------------------|----|-------|-------|------|-----|----|---------|--------------|
+	 No Hit    Short Desc               Prob E-value P-value  Score    SS Cols Query HMM  Template HMM
+	"""
+	
 	pattern = StringIO(template).readlines()[1]
 	colBreaks = [i for i, ch in enumerate(pattern) if ch == '|']
 	widths = [j-i for i, j in zip( ([0]+colBreaks)[:-1], colBreaks ) ]
@@ -52,8 +79,35 @@ def getFullDesc(hhresult_file,top_hit_full, verbose):
 
 	return full_desc
 
-def main():
+def displayRefs():
+    """Display relevant references"""
+    print('''
+    - The following publications pertain to HHSuite:
+	- Alva V., Nam SZ., Söding J., Lupas AN. (2016)
+	  The MPI bioinformatics Toolkit as an integrative platform for advanced protein sequence and structure analysis.
+	  Nucleic Acids Res. pii: gkw348. PMID: 27131380
+	
+	- Remmert M., Biegert A., Hauser A., Söding J. (2011) HHblits: Lightning-fast iterative protein sequence searching by HMM-HMM alignment.
+	  Nat Methods. 9(2):173-5. doi: 10.1038/nmeth.1818. PMID: 22198341
+    
+	- Söding J., Biegert A., Lupas AN. (2005) The HHpred interactive server for protein homology detection and structure prediction.
+	  Nucleic Acids Res 33 (Web Server issue), W244-W248. PMID: 15980461
+	
+	- Söding J. (2005) Protein homology detection by HMM-HMM comparison.
+	  Bioinformatics 21: 951-960. PMID: 15531603
+	
+	- Hildebrand A., Remmert A., Biegert A., Söding J. (2009) Fast and accurate automatic structure prediction with HHpred.
+	  Proteins 77(Suppl 9):128-32. doi: 10.1002/prot.22499. PMID: 19626712
+	
+	- Meier A., Söding J. (2015) Automatic Prediction of Protein 3D Structures by Probabilistic Multi-template Homology Modeling.
+	  PLoS Comput Biol. 11(10):e1004343. doi: 10.1371/journal.pcbi.1004343. PMID: 26496371
+	''')
+    sys.exit(1)
 
+def parseArgs():
+	"""Parse commandline arguments"""
+
+	import argparse
 	try:
    		parser = argparse.ArgumentParser(description='This script converts HHpreds verbose output in to a full table for subsequent analysis.')
 		parser.add_argument(
@@ -73,13 +127,26 @@ def main():
 			action='store',
 			default='None',
 			help='Output file name to store results in. If none provided, the default will be infile.tsv.')			
-					
+		parser.add_argument(
+			'-b',
+			'--bibliography',
+			action='store_true',
+			help='Display references.')
+							
 		args = parser.parse_args()
 		
 	except:
 		print "An exception occured with argument parsing. Check your provided options."
 		traceback.print_exc()
 
+	return parser.parse_args()
+
+def main():
+	"""Make function calls and handle arguments for tabulateHHpred.py"""
+	args = parseArgs()
+
+        if args.bibliography is True:
+                displayRefs()
 
 	verbose = args.verbose
 	hhresult_file = args.infile
@@ -88,9 +155,9 @@ def main():
 
 	split = os.path.splitext(args.infile)
 	basename = os.path.basename(split[0])
-
+	
 	if args.outfile is 'None':
-		outfile = indir+ '/' + basename + '.tsv'
+		outfile = indir + '/' + basename + '.tsv'
 	else:
 		outfile = args.outfile
 
