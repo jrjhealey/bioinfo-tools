e00a74ea65ba3b3805587642c903b37dc00d4a35
jrjhealey <jrj.healey@gmail.com>
2018-03-06 11:54:53 +0000

    update MSAnalysis todos

--- MSAnalysis.py
+++ MSAnalysis.py
@@ -11,7 +11,20 @@
 #    so that there are no ambiguous amino acids/bases
 # 2. Do logo/conservation code (report back proportions of the MSA that correspond to a given base/amino).
 # 3. Figure out the alphabet error for the information content function in Biopython.
-
+# 4. Implement a mutation caller based on the following code:
+	# import sys
+	# from Bio import SeqIO
+	#
+	# f = sys.argv[1]
+	#
+	# seq_records = SeqIO.parse(f, 'fasta')
+	# refseq_record = next(seq_records)
+	# for seq_record in seq_records:
+	#    for i in range(0, len(refseq_record)):
+	#        nt1 = refseq_record[i]
+	#        nt2 = seq_record[i]
+	#        if nt1 != nt2:
+	#            printseq_record.id, i+1, nt2, nt1)
 
 
 
