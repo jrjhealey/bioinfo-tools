9106f53156a7d684992fa348e7b98ec7d3e75dd8
jrjhealey <jrj.healey@gmail.com>
2019-01-28 16:10:36 +0000

    Refactor ACTgenerator and improve output/logic

--- ACTgenerator.sh
+++ ACTgenerator.sh
@@ -1,131 +1,130 @@
 #!/bin/bash
 set -eo pipefail
-
+tr=$(tput setaf 1) # text = red
+ty=$(tput setaf 3)
+tw=$(tput setaf 7)
+smul=$(tput smul)
+rmul=$(tput rmul)
+df=$(tput sgr0)    # text = reset
 # A small script to generate artemis comparison files (nucleic acid comparison)
 # since all the webservers are apparently defunct!
 
-# Script requires blastn (NOT LEGACY BLAST) and makeblastdb in path - check for existence:
+log(){
+  # Logging function.
+  # Prints to STDOUT in WHITE
+  echo -e >&1 "${tw}${smul}INFO:${rmul} $1${df}"
+}
 
-command -v makeblastdb >/dev/null 2>&1 || { echo >&2 "makeblastdb doesn't appear to be installed. Aborting."; exit 1; }
-command -v blastn >/dev/null 2>&1 || { echo >&2 "BLAST+ doesn't appear to be installed. Aborting."; exit 1; }
+err(){
+  # Error function
+  # Prints to STDERR in RED
+  echo -e >&2 "${tr}${smul}ERROR:${rmul} $1${df}"
+}
 
-# Capture inputs
-usage()
-{
-cat << EOF
+warn(){
+  # Warning function
+  # Prints to STDOUT in YELLOW/ORANGE
+  echo -e >&1 "${ty}${smul}WARNING:${rmul} $1${df}"
+}
+
+usage(){
+  # Capture inputs
+cat << EOF >&2
 usage: $0 options
 
 This script generates the necessary BLAST comparison file
 for use with the Artemis Comparison tool, when comparing 2 genomes.
 
+A typical invocation might look like:
+
+ $ bash ACTgenerator.sh -r reference.fasta -q query.fasta -d databasename -o outputdir -t "T"
+
 OPTIONS:
-   -h | --help     	Show this message
-   -r | --ref		Reference fasta sequence
-   -q | --query		Query fasta sequence
-   -d | --database	Name of the BLAST database comparison file.
-   -o | --outdir	Directory to output all the files to
-   -t | --tidy		Make the script tidy up after itself (T/F).
-			Default F.
+   -h | --help      Show this message.
+   -r | --ref       Reference fasta sequence.
+   -q | --query     Query fasta sequence.
+   -d | --database  Name of the BLAST database comparison file.
+   -o | --outdir    Directory to output all the files to.
+   -t | --tidy      Make the script tidy up after itself (T/F).
+                    ${tr}Default F.${df}
 
 EOF
 }
 
 # Tolerate long arguments
 for arg in "$@"; do
-  shift
-  case "$arg" in
-  		"--help")
-		set -- "$@" "-h"
-		;;
-	    	"--reference")
-    		set -- "$@" "-r"
-		;;
-   		"--query")
-    		set -- "$@" "-q"
-    		;;
-    		"--database")
-       		set -- "$@" "-d"
-       		;;
-		"--outdir")
-		set -- "$@" "-o"
-		;;
-		"--tidy")
-		set -- "$@" "-t"
-		;;
-	    	*)
-		set -- "$@" "$arg"
-  esac
+ shift
+ case "$arg" in
+   "--help")      set -- "$@" "-h"   ;;
+   "--reference") set -- "$@" "-r"   ;;
+   "--query")     set -- "$@" "-q"   ;;
+   "--database")  set -- "$@" "-d"   ;;
+   "--outdir")    set -- "$@" "-o"   ;;
+   "--tidy")      set -- "$@" "-t"   ;;
+   *)             set -- "$@" "$arg" ;;
+ esac
 done
 # getopts assigns the arguments to variables
 while getopts "hr:q:d:o:t:" OPTION ;do
   case $OPTION in
-		r)
-		reference=$OPTARG
-		;;
-		q)
-		query=$OPTARG
-		;;
-		d)
-		database=$OPTARG
-		;;
-		o)
-		outdir=$OPTARG
-		;;
-		t)
-		tidy=$OPTARG
-		;;
-		h)
-		usage
-		exit
-		;;
+    r) reference=$OPTARG ;;
+    q) query=$OPTARG     ;;
+    d) database=$OPTARG  ;;
+    o) outdir=$OPTARG    ;;
+    t) tidy=$OPTARG      ;;
+    h) usage; exit 0     ;;
 	esac
 done
 
+# If no args, show help
+if [[ $# -eq 0 ]] ; then
+    usage ; exit 1
+fi
+
 if [[ -z $reference ]]; then
-    	usage
-    	echo "No Reference sequence provided. Exiting." ; exit 1
+ usage ; err "No Reference sequence provided. Exiting." ; exit 1
 fi
 
 if [[ -z $query ]]; then
-    	usage
-    	echo "Query not supplied. Exiting." ; exit 1
+ usage ;  err "Query not supplied. Exiting." ; exit 1
 fi
 
 if [[ -z $database ]]; then
-    	database=${reference%.*}_DB
-	echo "No database name was specified. Using the reference sequence name and appending _DB."
+ database="${reference%.*}_DB"
+ warn "No database name was specified. Using the reference sequence name and appending _DB."
 fi
 
 if [[ -z $outdir ]]; then
-	outdir=$(pwd)
-	echo "No output directory was specified. Defaulting to the current working directory:"
-	pwd
-fi
-
-# Switch long options to short for tidy:
-if [ $tidy == "True" ]; then
- tidy="T"
- else
-  if [ $tidy == "False" ] || [ -z $tidy ]; then
-   tidy="F"
-  fi
+ outdir=$(pwd)
+ warn "No output directory was specified. Defaulting to the current working directory: $outdir"
 fi
 
 #####
 
 # Step 1: Make a BLAST database of the reference sequence:
+# Script requires blastn (NOT LEGACY BLAST) and makeblastdb in path - check for existence:
+command -v makeblastdb >/dev/null 2>&1 || { err "makeblastdb doesn't appear to be installed. Aborting."; exit 1; }
+command -v blastn >/dev/null 2>&1 || { err "BLAST+ doesn't appear to be installed. Aborting."; exit 1; }
 
-echo -en Executing command with arguments: '\n' Reference - "$reference" '\n' Database - "$database" '\n' Query sequence - "$query" '\n'
+
+log "Running makeblastdb:"
+log " -> makeblastdb -in "$reference" -dbtype 'nucl' -title "$database" -out "${outdir%/}"/"${database}" -parse_seqids"
 
 makeblastdb -in "$reference" -dbtype 'nucl' -title "$database" -out "${outdir%/}"/"${database}" -parse_seqids
-echo 'Database created.'
+log "Database created."
 
 # Step 2: Perform the all-vs-all BLAST using the query sequence and reference database.
+log " -> blastn -db "${outdir%/}"/"${database}" -query "$query" -outfmt 6 -out "${outdir%/}"/"${query%.*}"_vs_"${reference%.*}".act"
 blastn -db "${outdir%/}"/"${database}" -query "$query" -outfmt 6 -out "${outdir%/}"/"${query%.*}"_vs_"${reference%.*}".act
 
-echo 'All finished! The comparion file is called:'
-echo "${query%.*}_vs_${reference%/*}.act"
+log "All finished! The comparison file is called: ${query%.*}_vs_${reference%/*}.act"
 
-if [ $tidy == "T" ]; then
-	rm -v ${outdir%/}/${database}*
+if [[ $tidy =~ ^[Tt][Rr][Uu][Ee]$ ]] || [[ $tidy =~ ^[Tt]$ ]] ; then
+  warn "Tidying database files from ${outdir}."
+  rm -v "${outdir%/}"/"${database}"*
+elif [[ $tidy =~ ^[Ff][Aa][Ll][Ss][Ee]$ ]] || [[ $tidy =~ ^[Ff]$ ]] || [[ -z $tidy ]] ; then
+  :   # Do nothing if false-y
+else
+ err 'Unrecognised argument to tidy (should be T(rue), F(alse) or empty). Leaving files unmodified.'
 fi
+
