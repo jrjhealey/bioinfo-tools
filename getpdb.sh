38e8bdf74b9b16a9a5a37ba46bdd853655d5ec75
jrjhealey <jrj.healey@gmail.com>
2019-01-30 15:24:55 +0000

    Refactor getpdb.sh using tput and making use of pipe-able logs etc

--- getpdb.sh
+++ getpdb.sh
@@ -3,7 +3,39 @@ set -eo pipefail
 
 # Script to retrieve PDBs via the command line from the PDB HTTP/FTP
 
-# Capture inputs
+if [ -t 1 ] ; then
+  ncols=$(tput colors)
+  if [ -n "$ncols" ] && [ "$ncols" -ge 8 ] ; then
+    bold="$(tput bold)"
+    underline="$(tput smul)"
+    rmunderline="$(tput rmul)"
+    standout="$(tput smso)"
+    black="$(tput setaf 0)"
+    red="$(tput setaf 1)"
+    green="$(tput setaf 2)"
+    yellow="$(tput setaf 3)"
+    blue="$(tput setaf 4)"
+    magenta="$(tput setaf 5)"
+    cyan="$(tput setaf 6)"
+    white="$(tput setaf 7)"
+    default="$(tput sgr0)"
+  fi
+fi
+
+log(){
+  # Logging function (prints to STDOUT in WHITE).
+  echo -e >&1 "${white}${underline}INFO:${rmunderline} ${1:-$(</dev/stdin)}${default}"
+}
+
+err(){
+  # Error function (prints to STDERR in RED).
+  echo -e >&2 "${red}${underline}ERROR:${rmunderline} ${1:-$(</dev/stdin)}${default}"
+}
+
+warn(){
+  # Warning function (prints to STDOUT in YELLOW/ORANGE).
+  echo -e >&1 "${yellow}${underline}WARNING:${rmunderline} ${1:-$(</dev/stdin)}${default}"
+}
 
 usage()
 {
@@ -13,23 +45,14 @@ usage: $0 options
 This script retrieves PDB files from the Protein DataBank.
 
 OPTIONS:
-   -h | --help             Show this message
-   -i | --ID		   4-letter alphanumeric PDB ID(s) to fetch (e.g. 1abc)
-			   To download multiple structures, issue -i each time:
-			     ...  -i 1abc -i 2def -i 3ghi etc...
-   -m | --mode		   Which protocol to use to fetch with ("HTTP" or "FTP")
-   -o | --outdir	   Directory to move the PDB to (optional, default = ./):
-   			   $(pwd)
-
+   -h | --help      Show this message
+   -i | --ID        4-letter alphanumeric PDB ID(s) to fetch (e.g. 1abc)
+                    To download multiple structures, issue -i each time:
+                    ...  -i 1abc -i 2def -i 3ghi etc...
+   -m | --mode      Which protocol to use to fetch with ("HTTP" or "FTP")
+   -o | --outdir    Directory to move the PDB to (optional, default = ./):
+                    $(pwd)
 EOF
-}
-
-log(){
-echo -e >&1 "\033[32mINFO: $1\033[0m"
-}
-
-err(){
-echo -e >&2 "\033[31mERROR: $1\033[0m"
 }
 
 # Tolerate long arguments
@@ -49,15 +72,17 @@ while getopts "hi:m:o:" OPTION ; do
    i) ID+=($OPTARG)   ;;  # appends an array rather than set a single variable
    m) mode=$OPTARG    ;;
    o) outdir=$OPTARG  ;;
-   h) usage && exit 1 ;;
+   h) usage ; exit 1 ;;
   esac
 done
 shift $((OPTIND -1))
 
 # Check var and set defaults if necessary
+
+
 if [[ -z $ID ]] ; then
  usage
- echo "No ID was provided. Exiting." ; exit 1
+ err "No ID was provided. Exiting." ; exit 1
 fi
 
 if [[ -z "$mode" ]] ; then
@@ -66,24 +91,25 @@ fi
 
 if [[ -z "$outdir" ]] ; then
  outdir=$(pwd)
- echo "Saving in to ${outdir}"
+ log "Saving in to: ${outdir}"
 fi
 
 log "Fetching the following IDs:"
-printf '%s\n' "${ID[@]}"
-# TODO:
-#  Add logic for retrieval from mmCIF?
+log "${ID[*]}"
+
 for i in "${ID[@]}" ; do
 log "Now working on ${i}..."
  if [[ "$mode" == "FTP" ]]
   then
       i=$(echo "$i" |tr '[:upper:]' '[:lower:]')
-      wget ftp://ftp.wwpdb.org/pub/pdb/data/structures/all/pdb/pdb${i}.ent.gz || { err "Failed to find a matching PDB entry. Exiting." ; exit 1; }
-      gunzip pdb${i}.ent.gz &&  mv -v pdb${i}.ent ${outdir%./}/${i}.pdb
+      wget ftp://ftp.wwpdb.org/pub/pdb/data/structures/all/pdb/pdb${i}.ent.gz 2>&1 | sed '2,$s/^/\      /g' | log ||
+        { err "Failed to find a matching PDB entry. Exiting." ; exit 1; }
+      gunzip pdb${i}.ent.gz &&  mv -v pdb${i}.ent ${outdir%./}/${i}.pdb | sed '2,$s/^/\         /g' | log
   else
  if [[ "$mode" == "HTTP" ]]
   then
-      wget https://files.rcsb.org/download/${i}.pdb || { err "Failed to find a matching PDB entry. Exiting" ; exit 1; }
+      wget https://files.rcsb.org/download/${i}.pdb 2>&1 | sed '2,$s/^/\      /g' | log ||
+        { err "Failed to find a matching PDB entry. Exiting" ; exit 1; }
   fi
  fi
 done
