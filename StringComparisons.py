72450f6b55e8035d5326ef40c3d6cad99e268aa7
Joe Healey <jrj.healey@gmail.com>
2020-02-14 15:08:35 +0000

    perc ID function and Black'd source

--- StringComparisons.py
+++ StringComparisons.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+# /usr/bin/env python
 
 # Calculate similarity scores between sets of sequences via different metrics.
 # Requires BioPython as the only non-standard module.
@@ -13,81 +13,121 @@ from collections import Counter
 from Bio import AlignIO
 
 __author__ = "Joe R. J. Healey"
-__version__ = "1.1"
+__version__ = "1.2"
 __title__ = "StringComparisons.py"
 __license__ = "GPLv3"
 __author_email__ = "jrj.healey@gmail.com"
 
-#TODO:
+# TODO:
 # Add regex/fuzzy matching to the distance functions?
 # Add control/logic for case-sensitivity
 
+
 def get_args():
     """Parse command line arguments"""
-    desc = 'Perform various string comparisons using different metrics.'
-    epi = ('This is a little script to perform various string comparisons '
-          'between elements of a sequence alignment')
+    desc = "Perform various string comparisons using different metrics."
+    epi = (
+        "This is a little script to perform various string comparisons "
+        "between elements of a sequence alignment"
+    )
 
     try:
-        parser = argparse.ArgumentParser(description=desc, epilog=epi, prog='StringComparisons.py')
-
-        parser.add_argument('-a',
-                            '--alignment',
-                            action='store',
-                            help='The multiple sequence alignment (MSA) in any of the formats supported by Biopython\'s AlignIO.')
-	parser.add_argument('-f',
-			    '--format',
-			    action='store',
-			    default='fasta',
-			    help='The format of the sequence alignment, if not the default = FASTA.')
-        parser.add_argument('-m',
-                            '--method',
-                            action='store',
-                            choices=['hamming','cosine','levenshtein'],
-                            default='levenshtein',
-                            metavar="METHOD",
-                            help='What type of string comparison measure to return {hamming|cosine|levenshtein} [default = levenshtein]'
-                                 'If cosine is chosen, an optional kmer length is needed via -k|--kmer')
-        parser.add_argument('-k',
-                            '--kmer',
-                            type=int,
-                            default=5,
-                            help='Kmer length for use with method = "cosine".')
-	parser.add_argument('-A',
-			    '--stringA',
-			    type=str,
-			    action='store',
-			    help='Pass the first string to be compared directly as text.')
-	parser.add_argument('-B',
-			    '--stringB',
-			    type=str,
-			    action='store',
-			    help='Pass the second string to be compared directly as text.')
-	parser.add_argument('--average',
-			    action='store_false',
-			    help='Average the distance between all sequences (for MSAs).')
-        parser.add_argument('-v',
-                            '--verbose',
-                            action='store_true',
-                            help='Prints additional progress messages.')
+        parser = argparse.ArgumentParser(
+            description=desc, epilog=epi, prog="StringComparisons.py"
+        )
+        parser.add_argument(
+            "-a",
+            "--alignment",
+            action="store",
+            help="A multiple sequence alignment to analyse (MSA)."
+            "Valid formats are any of those supported by Biopython's AlignIO.",
+        )
+        parser.add_argument(
+            "-f",
+            "--format",
+            action="store",
+            default="fasta",
+            help="The format of the sequence alignment, if not the default = FASTA.",
+        )
+        parser.add_argument(
+            "-m",
+            "--method",
+            action="store",
+            choices=["hamming", "cosine", "levenshtein", "percent_id"],
+            default="levenshtein",
+            metavar="METHOD",
+            help="What type of string comparison measure to return"
+            "{hamming|cosine|levenshtein} [default = levenshtein]"
+            "If cosine is chosen, an optional kmer length is needed via -k|--kmer",
+        )
+        parser.add_argument(
+            "-k",
+            "--kmer",
+            type=int,
+            default=5,
+            help='Kmer length for use with method = "cosine".',
+        )
+        parser.add_argument(
+            "-A",
+            "--stringA",
+            type=str,
+            action="store",
+            help="Pass the first string to be compared directly as text.",
+        )
+        parser.add_argument(
+            "-B",
+            "--stringB",
+            type=str,
+            action="store",
+            help="Pass the second string to be compared directly as text.",
+        )
+        parser.add_argument(
+            "--average",
+            action="store_false",
+            help="Average the distance between all sequences (for MSAs).",
+        )
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            action="store_true",
+            help="Prints additional progress messages.",
+        )
         if len(sys.argv) == 1:
             parser.print_help(sys.stderr)
             sys.exit(1)
 
     except NameError:
-        sys.stderr.write("An exception occurred with argument parsing. Check your provided options.")
+        sys.stderr.write(
+            "An exception occurred with argument parsing. Check your provided options."
+        )
         sys.exit(1)
 
     return parser.parse_args()
 
 
 def hamming_distance(s1, s2):
-     """Return the Hamming distance between equal-length sequences"""
-     if len(s1) != len(s2):
-         raise ValueError("Undefined for sequences of unequal length")
-     return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+    """Return the Hamming distance between equal-length sequences"""
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length")
+    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+
+def percent_id(s1, s2):
+    """"Return the percentage identity between two strings.
+       Strings must be the same length or aligned to the same length for the
+       underlying Hamming Distance calculation to work.
+   """
+    try:
+        hd = hamming_distance(s1, s2)
+    except ValueError:
+        raise ValueError("Sequences must be the same length and/or aligned.")
+    return round(float(((len(s1) - hd) * 100) / len(s1)), 2)
+
 
 def levenshtein_distance(s1, s2):
+    """Return the Levenshtein Distance for 2 strings.
+       Unequal string lengths are permitted for LD.
+    """
     if len(s1) < len(s2):
         return levenshtein_distance(s2, s1)
 
@@ -107,6 +147,7 @@ def levenshtein_distance(s1, s2):
 
     return previous_row[-1]
 
+
 def cosine_distance(s1, s2, k):
     """Compute the cosine difference of the strings as kmer vectors"""
     import re, math
@@ -120,21 +161,24 @@ def cosine_distance(s1, s2, k):
     intersection = set(vec1.keys()) & set(vec2.keys())
     numerator = sum([vec1[x] * vec2[x] for x in intersection])
 
-    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
-    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
+    sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
     denominator = math.sqrt(sum1) * math.sqrt(sum2)
     if not denominator:
         return 0.0
     else:
         return float(numerator) / denominator
 
+
 def apply_method(method, s1, s2, k):
     """Case switch for the selected method"""
     return {
-            'hamming': partial(hamming_distance, s1, s2),
-            'cosine': partial(cosine_distance, s1, s2, k),
-            'levenshtein': partial(levenshtein_distance, s1, s2)
-           }[method]()
+        "hamming": partial(hamming_distance, s1, s2),
+        "cosine": partial(cosine_distance, s1, s2, k),
+        "levenshtein": partial(levenshtein_distance, s1, s2),
+        "percent_id": partial(percent_id, s1, s2),
+    }[method]()
+
 
 def main():
     """Compute distances from a provided MSA or pair of strings."""
@@ -142,28 +186,40 @@ def main():
     args = get_args()
 
     if args.alignment is not None:
-        if args.verbose: print('Alignment found, returning all pairwise distances.')
+        if args.verbose:
+            print("Alignment found, returning all pairwise distances.")
         msa = AlignIO.read(args.alignment, args.format)
         dists = []
         seq1_list = []
         seq2_list = []
         for i in range(len(msa)):
-            for j in range(i+1, len(msa)):
-                dists.append(apply_method(args.method, str(msa[i].seq), str(msa[j].seq), k=args.kmer))
+            for j in range(i + 1, len(msa)):
+                dists.append(
+                    apply_method(
+                        args.method, str(msa[i].seq), str(msa[j].seq), k=args.kmer
+                    )
+                )
                 seq1_list.append(str(msa[i].seq))
                 seq2_list.append(str(msa[j].seq))
         for dist, seq1, seq2 in zip(dists, seq1_list, seq2_list):
-            print(str(dist) + '\t' + seq1 + '\t' + seq2)
+            print(str(dist) + "\t" + seq1 + "\t" + seq2)
         if args.average:
-            print("Average pairwise similarity between MSA: {}".format(sum(dists)/len(dists)))
+            print(
+                "Average pairwise similarity between MSA: {}".format(
+                    sum(dists) / len(dists)
+                )
+            )
     else:
-        if args.verbose: print("No MSA found, comparing strings instead.")
-	if args.stringA and args.stringB:
-	    result = apply_method(args.method, args.stringA, args.stringB, args.kmer)
-	    print(str(result) + '\t' + args.stringA + '\t' + args.stringB)
+        if args.verbose:
+            print("No MSA found, comparing strings instead.")
+        if args.stringA and args.stringB:
+            result = apply_method(args.method, args.stringA, args.stringB, args.kmer)
+            print(str(result) + "\t" + args.stringA + "\t" + args.stringB)
         else:
-	    sys.stderr.write("Strings A and B are undefined, ensure both are provided with -A/--stringA and -B/--stringB")
+            sys.stderr.write(
+                "Strings A and B are undefined, ensure both are provided with -A/--stringA and -B/--stringB"
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
